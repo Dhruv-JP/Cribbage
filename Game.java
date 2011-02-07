c273e26aef8babfd847cc1370d4ef03745a1a0c1
Mike Sherman <mike.sher08@gmail.com>
2011-02-07 15:10:21 -0500

    only outputs who wins

--- Game.java
+++ Game.java
@@ -2,6 +2,8 @@ import java.util.*;
 import java.math.*;
 public class Game{
 	public static void main (String args[]){
+	int p1Win, p2Win;
+	p1Win = p2Win = 0;
 		while(true){
 			Cribbage game = new Cribbage();
 			int inProgress = 0;
@@ -11,6 +13,15 @@ public class Game{
 				Thread.currentThread().sleep(3000);
 			}catch(InterruptedException e){
 			}*/
+			}
+			if(inProgress == 1)
+				p1Win++;
+			else
+				p2Win++;
+			if(p2Win == 100 && p1Win < 100){
+				System.out.println("Player 1: "+p1Win);
+				System.out.println("Player 2: "+p2Win);
+				p1Win = p2Win = 0;
 			}
 		}
 	}
